4b1bca50b2a02d660facec2e03804d8733521faa
Kacper Kozak <kacper.kozak@siili.com>
2019-02-07 17:36:00 +0100

    Animate fragment width

--- src/components/Code/Frag.tsx
+++ src/components/Code/Frag.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useRef } from 'react'
 import styled from 'styled-components'
+import useComponentSize from '@rehooks/component-size'
 
 type CodeProps = {
     id: string
@@ -8,19 +9,28 @@ type CodeProps = {
     show?: boolean
 }
 
-export const Frag = (props: CodeProps) => (
-    <Code key={props.id} show={props.show}>
-        {props.code}
-    </Code>
-)
+export const Frag = (props: CodeProps) => {
+    const ref = useRef(null)
+    const { width } = useComponentSize(ref)
+
+    return (
+        <Code
+            key={props.id}
+            show={props.show}
+            ref={width ? null : ref}
+            style={{ width: width ? (props.show ? width : 0) : undefined }}
+        >
+            {props.code}
+        </Code>
+    )
+}
 
 const Code = styled.code<{ show?: boolean }>`
     transition: all 0.5s ease;
-    overflow: hidden;
     display: inline-block;
     vertical-align: top;
     color: #abc123;
     opacity: ${p => (p.show ? 1 : 0)};
-    width: ${p => (p.show ? 'auto' : 0)};
+    position: ${p => (p.show ? 'relative' : 'absolute')};
     transform: scale(${p => (p.show ? 1 : 0.5)});
 `
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
 
 module.exports = {
     entry: './src/index.tsx',
@@ -26,5 +27,6 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src', 'index.html'),
         }),
+        new DashboardPlugin(),
     ],
 }
