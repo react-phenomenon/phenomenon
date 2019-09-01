32e0fbf6046863c224c67ccca32b338896168667
Kacper Kozak <kacperkk@gmail.com>
2019-09-01 16:50:25 +0200

    Rewrite to GSAP

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,39 +1,25 @@
 import React from 'react'
 import { Deck } from '../components/Deck'
-import { Fade } from '../components/Fade'
 import { Slide } from '../components/Slide'
 import {
     backgroundColor,
     backgroundImage,
     Logo,
     mainBackgroundImage,
-    SubTitle,
     Text,
 } from '../themes/storm'
-import { Expand } from '../components/Expand'
-import { HowToUse } from './HowToUse'
-import { FullExample } from './FullExample'
 import { ConsoleExample } from './ConsoleExample'
+import { CssExample } from './CssExample'
+import { FullExample } from './FullExample'
+import { HowToUse } from './HowToUse'
 import { JavaScriptExample } from './JavaScriptExample'
 import { ReactExample } from './ReactExample'
-import { CssExample } from './CssExample'
 
 export const App = () => (
     <Deck config={{ backgroundImage, backgroundColor }}>
         <Slide config={{ backgroundImage: mainBackgroundImage }}>
             <Logo />
             <Text align="center">Click space or use arrow keys to navigate</Text>
-        </Slide>
-        <Slide>
-            <SubTitle>Fade component</SubTitle>
-            <Fade in={1}>
-                <Text align="center">Click space or use arrow keys to navigate</Text>
-            </Fade>
-            <Expand in={2} out={3}>
-                <Text align="center">
-                    <Logo />
-                </Text>
-            </Expand>
         </Slide>
         <Slide>
             <HowToUse />
