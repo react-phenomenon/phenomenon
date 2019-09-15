cd2a07b951e59f88b8e41fd19f584f4c21ea48df
Kacper Kozak <kacperkk@gmail.com>
2019-09-16 00:27:01 +0200

    Updates

---
+++ precache-manifest.eaf880f69f43add5ec8b8cdec710f2f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0529f5cbc20f71b059e16258516ce9",
+    "url": "/slides/index.html"
+  },
+  {
+    "revision": "a479ceccf99a16d145c3",
+    "url": "/slides/static/js/2.2c86b548.chunk.js"
+  },
+  {
+    "revision": "1d70109799f2b6cc3023",
+    "url": "/slides/static/js/main.2d87aca5.chunk.js"
+  },
+  {
+    "revision": "d532f4083349366303ae",
+    "url": "/slides/static/js/runtime~main.c9303d57.js"
+  },
+  {
+    "revision": "998f280dce8285a38ff8aae5ad7f5a3f",
+    "url": "/slides/static/media/background-blurred.998f280d.jpg"
+  },
+  {
+    "revision": "f19b53aa979537727a511732726c3b81",
+    "url": "/slides/static/media/background.f19b53aa.jpg"
+  },
+  {
+    "revision": "f3b1b96f0bcbb9dd027a4d888df92996",
+    "url": "/slides/static/media/logo.f3b1b96f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/slides/precache-manifest.5da86673d390ae1a4bf278601bf9e2be.js"
+  "/slides/precache-manifest.eaf880f69f43add5ec8b8cdec710f2f0.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/slides/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slides/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
